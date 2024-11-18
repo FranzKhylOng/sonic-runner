553a0909b38b87048b13540c7daab6804c59bf72
FranzKhyl <117539969+FranzKhylOng@users.noreply.github.com>
2024-11-18 11:23:47 +0800

    chore(scenes): use utils on play game

--- src/scenes/play-game.ts
+++ src/scenes/play-game.ts
@@ -4,44 +4,16 @@ import { MotoBug } from "../entities/motobug";
 import { Ring } from "../entities/ring";
 import { Sonic } from "../entities/sonic";
 import game from "../kaplayCtx";
+import { createBackgrounds, createPlatforms, setupParallax } from "../utils";
+import { bgWidth, platformHeight, platformWidth } from "../config";
 
 export function playGame() {
   game.setGravity(3100); //the gravity won't do anything if you do not have entities with a body component
 
-  const bgWidth = 1920;
-  const platformWidth = 1280;
   let score = 0;
 
-  const backgrounds = [
-    //game.add creates a new entity/object for the game
-    game.add([
-      game.sprite("chemicalBg"),
-      game.pos(0, 0),
-      game.scale(2),
-      game.opacity(0.8),
-    ]),
-    game.add([
-      game.sprite("chemicalBg"),
-      game.pos(bgWidth * 2, 0), //put the second bg piece next to the first one for parallax effect, 2x for scale
-      game.scale(2),
-      game.opacity(0.8),
-    ]),
-  ];
-
-  const platforms = [
-    game.add([
-      game.sprite("platforms"),
-      game.pos(0, 450),
-      game.scale(4),
-      game.area(),
-    ]),
-    game.add([
-      game.sprite("platforms"),
-      game.pos(platformWidth * 4, 450),
-      game.scale(4),
-      game.area(),
-    ]),
-  ];
+  const backgrounds = createBackgrounds(bgWidth);
+  const platforms = createPlatforms(platformWidth, platformHeight);
 
   const scoreText = game.add([
     game.text("Score: 0", { font: "mania", size: 72 }),
@@ -58,7 +30,7 @@ export function playGame() {
       game.destroy(enemy);
       sonic.play("jump");
       sonic.jump();
-      score = score + 5; //issue
+      score = score + 5; //issue doesnt count for some reason only rings
       scoreText.text = `Score: ${score}`;
 
       sonic.plusPoints!.text = "+5";
@@ -154,9 +126,7 @@ export function playGame() {
 
   spawnRing();
   spawnMotobug();
-  // if (gameSpeed > 1500) {
   spawnBuzzBomber();
-  // }
 
   game.add([
     game.rect(1, 1080),
@@ -182,29 +152,6 @@ export function playGame() {
     game.body({ isStatic: true }), //enables physics
   ]);
 
-  game.onUpdate(() => {
-    // Parallax effect when a foreground element moves at a different speed than a background element
-    // Check if the second bg piece is off screen
-    if (backgrounds[1].pos.x < -bgWidth) {
-      // Move the first bg piece to the right of the second bg piece
-      backgrounds[0].moveTo(backgrounds[1].pos.x + bgWidth * 2, 0);
-      const shiftedBg = backgrounds.shift(); // Remove the first bg piece from the array and return it
-      backgrounds.push(shiftedBg!); // Add the first bg piece to the end of the array to reset the indexes
-    }
-
-    backgrounds[0].move(-100, 0);
-    backgrounds[1].move(-100, 0);
-
-    if (platforms[1].pos.x < -platformWidth) {
-      platforms[0].moveTo(platforms[1].pos.x + platformWidth, 450);
-      const shiftedPlatform = platforms.shift();
-
-      if (shiftedPlatform) {
-        platforms.push(shiftedPlatform);
-      }
-    }
-
-    platforms[0].move(-gameSpeed, 0);
-    platforms[1].move(-gameSpeed, 0);
-  });
+  setupParallax(backgrounds, bgWidth, 100, 0);
+  setupParallax(platforms, platformWidth, gameSpeed, platformHeight);
 }
